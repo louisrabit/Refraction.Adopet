44a22a8ab67dcea8380e65a6c89301ee459666d2
Luis Manuel Brito Coelho <98318766+louisrabit@users.noreply.github.com>
2023-10-16 17:39:03 +0100

    Add project files.

---
+++ Alura.Adopet.Console/PathArchieve.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console;
+
+public class PathArchieve
+{
+    HttpClient client;
+
+    public PathArchieve()
+    {
+      this.client =  ConfiguraHttpClient("http://localhost:5057");
+
+    }
+
+    public async Task ImportArchieveAsync(string pathArchieve)
+    {
+        List<Pet> listaDePet = new List<Pet>();
+
+        using (StreamReader sr = new StreamReader(pathArchieve))
+        {
+            while (!sr.EndOfStream)
+            {
+                // separa linha usando ponto e vírgula
+                string[] propriedades = sr.ReadLine().Split(';');
+                // cria objeto Pet a partir da separação
+                Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                  propriedades[1],
+                  TipoPet.Cachorro
+                 );
+
+                System.Console.WriteLine(pet);
+                listaDePet.Add(pet);
+            }
+        }
+        foreach (var pet in listaDePet)
+        {
+            try
+            {
+                var resposta = await CreatePetAsync(pet);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        System.Console.WriteLine("Importação concluída!");
+    }
+    Task<HttpResponseMessage> CreatePetAsync(Pet pet)
+    {
+        HttpResponseMessage? response = null;
+        using (response = new HttpResponseMessage())
+        {
+            return client.PostAsJsonAsync("pet/add", pet);
+        }
+    }
+    HttpClient ConfiguraHttpClient(string url)
+    {
+        HttpClient _client = new HttpClient();
+        _client.DefaultRequestHeaders.Accept.Clear();
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        _client.BaseAddress = new Uri(url);
+        return _client;
+    }
+}
+
